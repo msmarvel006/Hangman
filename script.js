5423c6ba97160cb1804c237539ba64a61222354f
msmarvel006 <117928667+msmarvel006@users.noreply.github.com>
2024-04-29 03:51:51 +0000

    updates

--- script.js
+++ script.js
@@ -1,44 +1,60 @@
- let words = ["pickle", "pizza", "tacos", "fries", "cheese"];
+let words = ["pickle", "pizza", "tacos", "fries", "cheese"];
 
-let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let counter = 1;
 
-let hangmanWord = words[Math.floor(Math.random()*words.length)];
+let hangmanWord = words[Math.floor(Math.random() * words.length)];
 console.log(hangmanWord);
 //create a innerHTML to put the hangmanWord on the page
-let wordDisplay = document.querySelector(".word-header");
-console.log(wordDisplay);
-for(let i = 0; i < hangmanWord.length; i++) {
-  wordDisplay.append("<div class='game-box'><ul class='word-display" + i + "'> <li class='letter' style='display:block' id='blank'></li><li class = 'letter' style='display:none' id='"+ hangmanWord.substring(i, i+1) +"'>" + hangmanWord.substring(i, i+1) + "</li></ul></div>");
-  
+let wordDisplay = document.querySelector(".game-box");
+for (let i = 0; i < hangmanWord.length; i++) {
+  wordDisplay.innerHTML += "<ul class='word-display" + i + "'> <li class='letter' style='display: block; height: 30px;width: 30px;border-bottom: 3px solid black;' id='blank-" + hangmanWord.substring(i, i + 1) + "'></li><li class='letter' style='display: none; width: 30px;border-bottom: 3px solid black;' id='letter-" + hangmanWord.substring(i, i + 1) + "'>" + hangmanWord.substring(i, i + 1) + "</li></ul>";
+
 };
 
 
 //https://www.w3schools.com/jsref/event_onclick.asp
 
 let numWrong = 1;
+let numRight = 0;
 
 //Checks letters to see if they match a letter in the word
-function checkLetter(element, letter)
-{
-  if(hangmanWord.indexOf(letter) == -1)
-  {
+function checkLetter(element, letter) {
+  if (hangmanWord.indexOf(letter) == -1) {
     console.log(letter);
     element.style.background = "red";
     let idName = "hc-" + numWrong;
-    document.getElementById(idName).style.display = "none";
-    document.getElementById("blank").style.display = "none";
-    document.getElementById(letter).style.display = "block";
     console.log(idName);
-    
+    var element = document.getElementById(idName);
+    if (element != null) {
+      element.style.background = "white";
+    }
     numWrong++;
   }
-  else
-  {
+  else {
     element.style.background = "green";
+
+    let blanksToHide = document.querySelectorAll("#blank-" + letter);
+    for(let i=0; i<blanksToHide.length; i++){
+      blanksToHide[i].style.display = "none";
+    }
+
+    let lettersToDisplay = document.querySelectorAll("#letter-" + letter);
+    for(let i=0; i<lettersToDisplay.length; i++){
+      lettersToDisplay[i].style.display = "block";
+      numRight++;
+    }
+  }
+
+  if (numWrong > 7) {
+    document.querySelector('.header').innerHTML='GAME OVER';
+  }
+   console.log("numRight"+numRight);
+  if (hangmanWord.length == numRight) {
+    document.querySelector('.header').innerHTML='CORRECT!';
   }
 }
 
-    
-  
+
+
 
